ab89a95c5c5c4249060a1002cd7ff459801283bc
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-05-24 10:59:25 -0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wraith-king
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wraith_king/core_test.clj
@@ -0,0 +1,7 @@
+(ns wraith-king.core-test
+  (:require [clojure.test :refer :all]
+            [wraith-king.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
