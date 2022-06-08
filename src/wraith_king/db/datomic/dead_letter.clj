dc59ed9c2289dc9025a0474f091e2734b9da513a
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-06-08 20:24:39 -0300

    lookup dead-letter by id datomic fn

--- src/wraith_king/db/datomic/dead_letter.clj
+++ src/wraith_king/db/datomic/dead_letter.clj
@@ -7,3 +7,11 @@
   [dead-letter :- models.dead-letter/DeadLetter
    datomic]
   (d/transact datomic [dead-letter]))
+
+(s/defn lookup-by-dead-letter-id! :- (s/maybe models.dead-letter/DeadLetter)
+  [dead-letter-id :- s/Uuid
+   datomic]
+  (some-> (d/q '[:find (pull ?dead-letter [*])
+                 :in $ ?dead-letter-id
+                 :where [?dead-letter :dead-letter/id ?dead-letter-id]] (d/db datomic) dead-letter-id)
+          ffirst))
