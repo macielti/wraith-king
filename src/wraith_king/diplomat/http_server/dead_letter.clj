24138510ee442f5eb59199ad29c1038b98bc9bef
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-06-08 20:13:35 -0300

    add create! dead-letter http request handler fn

--- src/wraith_king/diplomat/http_server/dead_letter.clj
+++ src/wraith_king/diplomat/http_server/dead_letter.clj
@@ -1,9 +1,12 @@
 (ns wraith-king.diplomat.http-server.dead-letter
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s]
+            [wraith-king.controllers.dead-letter :as controllers.dead-letter]
+            [wraith-king.adapters.dead-letter :as adapters.dead-letter]))
 
 (s/defn create!
   "Create new dead-letter"
-  [{dead-letter                       :json-params
-    {:keys [datomic producer config]} :components}]
+  [{dead-letter       :json-params
+    {:keys [datomic]} :components}]
   {:status 200
-   :body   ()})
+   :body   (-> (adapters.dead-letter/wire->dead-letter dead-letter)
+               (controllers.dead-letter/create! datomic))})
