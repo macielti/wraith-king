2b19213d75e497fd72e6adc6a91805884f467591
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-07-15 16:33:44 -0300

    add consumer component

--- src/wraith_king/components.clj
+++ src/wraith_king/components.clj
@@ -4,6 +4,7 @@
             [common-clj.component.datomic :as component.datomic]
             [common-clj.component.routes :as component.routes]
             [common-clj.component.service :as component.service]
+            [common-clj.component.kafka.consumer :as component.consumer]
             [wraith-king.db.datomic.config :as datomic.config]
             [wraith-king.diplomat.http-server :as diplomat.http-server]))
 
@@ -13,6 +14,7 @@
     :config (component.config/new-config "resources/config.edn" :prod :edn)
     :datomic (component/using (component.datomic/new-datomic datomic.config/schemas) [:config])
     :routes (component/using (component.routes/new-routes diplomat.http-server/routes) [:datomic :config])
+    :consumer (component/using (component.consumer/new-consumer diplomat.consumer/consumers) [:config :datomic])
     :service (component/using (common-clj.component.service/new-service) [:routes :datomic :config])))
 
 (defn start-system! []
@@ -23,4 +25,5 @@
     :config (component.config/new-config "resources/config.example.edn" :test :edn)
     :datomic (component/using (component.datomic/new-datomic datomic.config/schemas) [:config])
     :routes (component/using (component.routes/new-routes diplomat.http-server/routes) [:datomic :config])
+    :consumer (component/using (component.consumer/new-mock-consumer diplomat.consumer/consumers) [:config :datomic])
     :service (component/using (component.service/new-service) [:routes :datomic :config])))
