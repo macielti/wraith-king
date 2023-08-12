5cd5e245f448bb0b55b5f1df3a7b494af1d42635
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-08-12 12:00:26 -0300

    port mark as processed dead-letters function

--- test/unit/wraith_king/db/datalevin/deadletter_test.clj
+++ test/unit/wraith_king/db/datalevin/deadletter_test.clj
@@ -3,12 +3,15 @@
             [datalevin.core :as d]
             [wraith-king.db.datalevin.deadletter :as database.deadletter]
             [schema.test :as s]
-            [fixtures.dead-letter]))
+            [fixtures.dead-letter]
+            [wraith-king.wire.datalevin.dead-letter :as wire.datalevin.dead-letter]
+            [matcher-combinators.test :refer [match?]])
+  (:import (java.util Date)))
 
 (s/deftest insert-and-lookup-test
   (testing "that we can insert a deadletter entity"
     (let [database-uri (datalevin.util/tmp-dir (str "query-or-" (random-uuid)))
-          database-connection (d/get-conn database-uri)]
+          database-connection (d/get-conn database-uri wire.datalevin.dead-letter/dead-letter-skeleton)]
       (database.deadletter/insert! fixtures.dead-letter/internal-dead-letter database-connection)
       (is (= {:dead-letter/id             fixtures.dead-letter/deadletter-id
               :dead-letter/exception-info "Critical Exception (StackTrace)"
@@ -24,9 +27,22 @@
 (s/deftest active-test
   (testing "that we can query all active dead-letters"
     (let [database-uri (datalevin.util/tmp-dir (str "query-or-" (random-uuid)))
-          database-connection (d/get-conn database-uri)]
+          database-connection (d/get-conn database-uri wire.datalevin.dead-letter/dead-letter-skeleton)]
       (database.deadletter/insert! fixtures.dead-letter/internal-dead-letter database-connection)
       (database.deadletter/insert! fixtures.dead-letter/internal-processed-dead-letter database-connection)
       (database.deadletter/insert! fixtures.dead-letter/internal-dropped-dead-letter database-connection)
       (is (= [fixtures.dead-letter/internal-dead-letter]
              (database.deadletter/active (d/db database-connection)))))))
+
+(s/deftest mask-as-processed-test
+  (testing "that we can mark a unprocessed dead-letter as processed"
+    (let [database-uri (datalevin.util/tmp-dir (str "query-or-" (random-uuid)))
+          database-connection (d/get-conn database-uri wire.datalevin.dead-letter/dead-letter-skeleton)]
+      (database.deadletter/insert! fixtures.dead-letter/internal-dead-letter database-connection)
+      (database.deadletter/mask-as-processed! fixtures.dead-letter/internal-dead-letter
+                                              database-connection)
+      (is (match? (assoc fixtures.dead-letter/internal-dead-letter
+                    :dead-letter/replay-count 1
+                    :dead-letter/status :processed
+                    :dead-letter/updated-at (fn [update-at] (type Date) update-at))
+                  (database.deadletter/lookup fixtures.dead-letter/deadletter-id (d/db database-connection)))))))
